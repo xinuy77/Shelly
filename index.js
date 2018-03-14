df182285f988350345c9bde9c4d43c5bee3a17fe
stevenfangcu <stevenfang.cu@gmail.com>
2018-03-14 18:32:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { spawn } = require('child_process');
 var pty         = require('node-pty');
 var Terminal    = require('xterm').Terminal;
-var commands    = ["Sad", "Happy", "Mad"];
+var commands    = ["Sad", "Happy", "Mad"]; // just a random array for the commands that we'll be implementing and using as the
+                                           // database for now 
 
 const shell =  new Terminal();
 
 shell.open(document.getElementById('shell'));
 
+//this'll catch if the user presses a key so it'll parse the input and see if it can be auto-completed through our command array
 document.getElementByID('xterm-helper-textarea').addEventListener("onkeypress", functionName);
 
 var ptyProcess = pty.spawn('bash', [], {
