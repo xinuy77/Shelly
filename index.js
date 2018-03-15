b16c5e18efe349952ac2852dd6c9a03e454cfe91
xinuy77 <yuyamanaka@cmail.carleton.ca>
2018-03-15 11:07:10 -0500

    cleaned

--- index.js
+++ index.js
@@ -9,31 +9,38 @@ Terminal.applyAddon(fit);
 const shell    = new Terminal();
 var   commands = ["Sad", "Happy", "Mad"]; // just a random array for the commands that we'll be implementing and using as the
                                            // database for now 
-
-shell.open($('#shell')[0]);
-
-shell.fit();
-
+    
 //this'll catch if the user presses a key so it'll parse the input and see if it can be auto-completed through our command array
 //document.getElementByID('xterm-helper-textarea').addEventListener("onkeypress", functionName);
 
-var ptyProcess = pty.spawn('bash', [], {
-    name: 'xterm-color',
-    cols: 80,
-    rows: 30,
-    cwd: process.cwd(),
-    env: process.env
-});
+$(document).ready(function() {
+    var ptyProcess = pty.spawn('bash', [], {
+        name: 'xterm-color',
+        cols: 80,
+        rows: 80,
+        cwd: process.cwd(),
+        env: process.env
+    });
 
-shell.on('data', function(data){
-    ptyProcess.write(data);
-});
-
-ptyProcess.on('data', function(data) {
-    shell.write(data);
-});
-   
-window.addEventListener('resize', function(e){
+    shell.open($('#shell')[0]);
     shell.fit();
+
+    setResizeEvent();
+    setStreamPipeEvent(ptyProcess);   
 });
 
+function setStreamPipeEvent(ptyProcess) {
+    shell.on('data', function(data){
+        ptyProcess.write(data);
+    });
+
+    ptyProcess.on('data', function(data) {
+        shell.write(data);
+    });
+};
+
+function setResizeEvent() {
+    window.addEventListener('resize', function(e){
+        shell.fit();
+    });
+};
