b9aa279adc1510a44ebe04e58d6eee726cdd84d4
stevenfangcu <stevenfang.cu@gmail.com>
2018-04-07 21:16:24 -0400

    Update time.js

--- utils/time.js
+++ utils/time.js
@@ -4,37 +4,21 @@ function updateTime(){
     var minutes = currentTime.getMinutes()
     var seconds = currentTime.getSeconds();
     var filler;
-    var checkBox = document.getElementById("myCheck");
-      if(checkBox.checked == true){
-        if (minutes < 10){
+    if (minutes < 10){
             minutes = "0" + minutes
-        }
+        }   
         if(seconds < 10){
           seconds = "0" + seconds;
-        }
+        } 
         if(hours > 13){
           hours = hours - 12;
-        }
+        } 
         var t_str = hours + ":" + minutes + ":" + seconds + " ";
         if(hours > 11){
             t_str += "PM";
         } else {
             t_str += "AM";
-        }
-      }else{
-        if (minutes < 10){
-            minutes = "0" + minutes
-        }
-        if(seconds < 10){
-          seconds = "0" + seconds;
-        }
-        var t_str = hours + ":" + minutes + ":" + seconds + " ";
-        if(hours > 11){
-            t_str += "PM";
-        } else {
-            t_str += "AM";
-        }
-    }
+        }   
     document.getElementById('time').innerHTML = t_str;
-}
+}   
 setInterval(updateTime, 1000);
